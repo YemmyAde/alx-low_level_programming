95358cdf87516d43a3cb0058093490bd538388e7
YemmyAde <yemisadesanya@gmail.com>
2023-03-05 13:35:13 -0800

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+*Description: print base 16 digits
+*
+*Return: return 0
+*/
+
+int main(void)
+{
+	char num = '0';
+	char alp = 'a';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+
+	while (alp <= 'g')
+	{
+		putchar(alp);
+		alp++;
+	}
+
+	putchar('\n');
+	return (0);
+
+}
