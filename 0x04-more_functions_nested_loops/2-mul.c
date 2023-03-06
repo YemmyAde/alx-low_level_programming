8e1ae20a036a78fd9c1411b07d021f9006f89a75
YemmyAde <yemisadesanya@gmail.com>
2023-03-06 13:58:40 -0800

    function thet return multiplied value

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*mul - function multiplies 2 integer
+*@a: first paramaters of my function
+*@b: second parameter of my fuction
+*Return: a multiplied by b
+*
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
